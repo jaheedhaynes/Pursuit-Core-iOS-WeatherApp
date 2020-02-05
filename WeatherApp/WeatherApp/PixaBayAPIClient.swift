6012cc9eb5fe9b31e4f7575b0b8bc9d3b5dedc05
Jaheed Haynes <jaheedhaynes@pursuit.org>
2020-02-05 09:50:15 -0500

    test

---
+++ WeatherApp/WeatherApp/PixaBayAPIClient.swift
@@ -0,0 +1,9 @@
+//
+//  PixaBayAPIClient.swift
+//  WeatherApp
+//
+//  Created by Jaheed Haynes on 2/5/20.
+//  Copyright © 2020 David Rifkin. All rights reserved.
+//
+
+import Foundation
---
+++ WeatherApp/WeatherApp/WeatherAPIClient.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherAPIClient.swift
+//  WeatherApp
+//
+//  Created by Jaheed Haynes on 2/5/20.
+//  Copyright © 2020 David Rifkin. All rights reserved.
+//
+
+import Foundation
